766557ad20ffe671ed02ba37aed8acea6b341d8f
BSPB0962 <https://github.com/GoncharovaTA/bspb_java>
2021-02-28 23:42:39 +0300

    java_lesson_4

--- src/ru/bspb/lesson4/ObjectMain1.java
+++ src/ru/bspb/lesson4/ObjectMain1.java
@@ -43,8 +43,8 @@ public class ObjectMain1 {
         PhysicClient[] clients = new PhysicClient[10];
         for (int i = 0; i < 10; i++) {
             clients[i] = new PhysicClient(name[i], age[i], gender[i]);
- //           clients[i].setName(name[i]);
- //           clients[i].setAge(age[i]);
+            //           clients[i].setName(name[i]);
+            //           clients[i].setAge(age[i]);
 //            clients[i].setGender(gender[i]);
         }
         Utils.sortByAges(clients);
@@ -53,5 +53,19 @@ public class ObjectMain1 {
 
         Utils.print(withoutDuplicate);
     }
+
+    IPClient[] ipClient = new IPClient[]{
+            new IPClient("ИП Винни-Пух Иван Георгиевич", 35, 123456789012345L),
+            new IPClient("ИП Пятачек Семен Аркадиевич", 23, 123456789054321L),
+            new IPClient("ИП Осликович Армен Зуевич", 59, 123456789045612L),
+            new IPClient("ИП Сова Клавдия Петровна", 78, 123456789059578L),
+            new IPClient("ИП Тигра Лариса Викторовна", 31, 123456789025896L),
+            new IPClient("ИП Карлсон Юрий Валентинович", 45, 123456789023145L),
+            new IPClient("ИП Фрекен-Бок Мария Игоревна", 68, 123456789036974L),
+            new IPClient("ИП Малыш Дмиртий Алексеевич", 18, 123456789025698L),
+            new IPClient("ИП Буратино Карл Феликсович", 27, 123456789014385L),
+            new IPClient("ИП Мальвина Инесса Аркадиевна", 25, 123456789014587L),
+    };
 }
+
 
